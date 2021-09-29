05fbf811716668cf9fbc2300e82979deb8d4f868
Your Name <you@example.com>
2021-09-29 14:46:25 +0300

    3 approaches to chats creation

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import EventSourcing from "./EventSourcing";
+import Longpulling from "./Longpulling";
+import WebSockett from "./WebSocket";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Longpulling /> */}
+      {/* <EventSourcing /> */}
+      <WebSockett />
+    </div>
+  );
+}
+
+export default App;
